a7015356923084d53435ed40e9e1c989a06704d5
Simon Polunin IVBO-03-20 <90776399+Sailiras@users.noreply.github.com>
2021-09-15 21:27:00 +0300

    Initial commit

---
+++ src/ru/mirea/test/Main.java
@@ -0,0 +1,11 @@
+package ru.mirea.test;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hi");
+        System.out.println("Tester");
+        System.out.println("Third");
+    }
+}
